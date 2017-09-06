8a13a9d11f44c82d7aa7e3e054b1e6f89650d487
sakharov <mail@asakharov.ru>
2017-09-06 19:54:34 +0300

    Canonical citation postprocessing

--- parsers.py
+++ parsers.py
@@ -1,6 +1,7 @@
 import re
 import logging
 import nltk
+import string
 
 MIDDLE_DASH_BETWEEN_DIGITS_REGEXP = re.compile(r'(\d+)\s*[-—–]\s*(\d+)', re.MULTILINE)
 
@@ -13,114 +14,168 @@ def middle_dash_between_digits(text):
 
 
 CANONIC_DICT = {
-    'Быт.': 'Быт.',
-    'Исх.': 'Исх.',
-    'Лев.': 'Лев.',
-    'Чис.': 'Чис.',
-    'Числ.': 'Чис.',
-    'Втор.': 'Втор.',
-    'Нав.': 'Нав.',
-    'Суд.': 'Суд.',
-    'Руф.': 'Руф.',
-    '1 Цар.': '1Цар.',
-    '2 Цар.': '2Цар.',
-    '3 Цар.': '3Цар.',
-    '4 Цар.': '4Цар.',
-    '1 Пар.': '1Пар.',
-    '2 Пар.': '2Пар.',
-    '1 Паралип.': '1Пар.',
-    '2 Паралип.': '2Пар.',
-    '1 Ездр.': '1Ездр.',
-    'Неем.': 'Неем.',
-    '2 Ездр.': '2Ездр.',
-    'Тов.': 'Тов.',
-    'Иудиф.': 'Иудиф.',
-    'Эсф.': 'Эсф.',
-    '1 Мак.': '1 Мак.',
-    '2 Мак.': '2Мак.',
-    '3 Мак.': '3Мак.',
-    '3 Ездр.': '3Ездр.',
-    'Иов.': 'Иов.',
-    'Пс.': 'Пс.',
-    'Притч.': 'Притч.',
-    'Еккл.': 'Еккл.',
-    'Песн.': 'Песн.',
-    'Прем.': 'Прем.',
-    'Сир.': 'Сир.',
-    'Ис.': 'Ис.',
-    'Иерем.': 'Иер.',
-    'Вар.': 'Вар.',
-    'Иезек.': 'Иез.',
-    'Дан.': 'Дан.',
-    'Ос.': 'Ос.',
-    'Иоил.': 'Иоил.',
-    'Ам.': 'Ам.',
-    'Авд.': 'Авд.',
-    'Ион.': 'Ион.',
-    'Мих.': 'Мих.',
-    'Наум.': 'Наум.',
-    'Авв.': 'Авв.',
-    'Соф.': 'Соф.',
-    'Агг.': 'Агг.',
-    'Зах.': 'Зах.',
-    'Мал.': 'Мал.',
-    'Мф.': 'Мф.',
-    'Матф.': 'Мф.',
-    'Мк.': 'Мк.',
-    'Мрк.': 'Мк.',
-    'Марк.': 'Мк.',
-    'Лк.': 'Лк.',
-    'Лук.': 'Лк.',
-    'Ин.': 'Ин.',
-    'Иоан.': 'Ин.',
-    'Иоанн.': 'Ин.',
-    'Деян.': 'Деян.',
-    'Иак.': 'Иак.',
-    '1 Петр.': '1Петр.',
-    '2 Петр.': '2Петр.',
-    '1 Иоан.': '1Иоан.',
-    '2 Иоан.': '2Иоан.',
-    '3 Иоан.': '3Иоан.',
-    'Иуд.': 'Иуд.',
-    'Рим.': 'Рим.',
-    '1 Кор.': '1Кор.',
-    '2 Кор.': '2Кор.',
-    'Гал.': 'Гал.',
-    'Галл.': 'Галл.',
-    'Еф.': 'Еф.',
-    'Ефес.': 'Еф.',
-    'Флп.': 'Флп.',
-    'Кол.': 'Кол.',
-    'Колос.': 'Кол.',
-    '1 Сол.': '1Сол.',
-    '2 Сол.': '2Сол.',
-    '1 Фес.': '1Фес.',
-    '1 Фесс.': '1Фес.',
-    '2 Фес.': '2Фес.',
-    '2 Фесс.': '2Фес.',
-    'Тим.': 'Тим.',
-    '1 Тимоф.': '1Тимоф.',
-    '1 Тим.': '1Тим.',
-    '2 Тимоф.': '2Тимоф.',
-    '2 Тим.': '2 Тим.',
-    'Тит.': 'Тит.',
-    'Филим.': 'Флм.',
-    'Евр.': 'Евр.',
-    'Апок.': 'Апок.',
-    'Откр.': 'Откр.',
-    'Сирах.': 'Сирах.'}
+    'Быт': 'Быт',
+    'Исх': 'Исх',
+    'Лев': 'Лев',
+    'Чис': 'Чис',
+    'Числ': 'Чис',
+    'Втор': 'Втор',
+    'Нав': 'Нав',
+    'Суд': 'Суд',
+    'Руф': 'Руф',
+    'Цар': 'Цар',
+    'Пар': 'Пар',
+    'Паралип': 'Пар',
+    'Неем': 'Неем',
+    'Тов': 'Тов',
+    'Иудиф': 'Иудиф',
+    'Эсф': 'Эсф',
+    'Мак': 'Мак',
+    'Ездр': 'Ездр',
+    'Иов': 'Иов',
+    'Пс': 'Пс',
+    'Притч': 'Притч',
+    'Еккл': 'Еккл',
+    'Песн': 'Песн',
+    'Прем': 'Прем',
+    'Сир': 'Сир',
+    'Ис': 'Ис',
+    'Иерем': 'Иер',
+    'Вар': 'Вар',
+    'Иезек': 'Иез',
+    'Дан': 'Дан',
+    'Ос': 'Ос',
+    'Иоил': 'Иоил',
+    'Ам': 'Ам',
+    'Авд': 'Авд',
+    'Ион': 'Ион',
+    'Мих': 'Мих',
+    'Наум': 'Наум',
+    'Авв': 'Авв',
+    'Соф': 'Соф',
+    'Агг': 'Агг',
+    'Зах': 'Зах',
+    'Мал': 'Мал',
+    'Мф': 'Мф',
+    'Матф': 'Мф',
+    'Мк': 'Мк',
+    'Мрк': 'Мк',
+    'Марк': 'Мк',
+    'Лк': 'Лк',
+    'Лук': 'Лк',
+    'Ин': 'Ин',
+    'Иоанн': 'Ин',
+    'Деян': 'Деян',
+    'Иак': 'Иак',
+    'Петр': 'Петр',
+    'Иоан': 'Ин',
+    'Иуд': 'Иуд',
+    'Рим': 'Рим',
+    'Кор': 'Кор',
+    'Гал': 'Гал',
+    'Галл': 'Галл',
+    'Еф': 'Еф',
+    'Ефес': 'Еф',
+    'Флп': 'Флп',
+    'Кол': 'Кол',
+    'Колос': 'Кол',
+    'Сол': 'Сол',
+    'Фес': 'Фес',
+    'Фесс': 'Фес',
+    'Тим': 'Тим',
+    'Тимоф': 'Тимоф',
+    'Тит': 'Тит',
+    'Филим': 'Флм',
+    'Евр': 'Евр',
+    'Апок': 'Откр',
+    'Откр': 'Откр',
+    'Сирах': 'Сирах'}
 
 
 def canonic_links(text):
-    keywords = set((key[:-1] for key in CANONIC_DICT.keys()))
-    keywords.update((key[:-1] for key in CANONIC_DICT.values()))
+    '''
+    Silly descent parser for canonic licks
 
-    words = nltk.word_tokenize(text)
-    for i, word in enumerate(words):
-        if word in keywords:
-            if i > 10:
-                print(words[i-10:i+10])
+    :param text: text to parse
+    :return: parsed text
+    '''
+    def format(token_list, index):
+        result = {'result': '', 'context': token_list, 'start_index': index}
+
+        if not token_list[index+1] == '.':  # dot should always be
+            logging.warning('[WARN] Undotted: %s', token_list[index:index + 5])
+            return
+
+        if token_list[index] not in CANONIC_DICT:
+            logging.warning('[WARN] Not in dict: %s', token_list[index:index + 5])
+            return
+
+        if token_list[index - 1].isdecimal():  # 1Петр.
+            result['result'] += str(token_list[index - 1])
+        else:
+            result['start_index'] += 1
+
+        result['result'] += CANONIC_DICT[token_list[index]]
+
+        while index < len(token_list):
+            index += 1
+            token = token_list[index]
+
+            if token == ')':
+                break
+            elif token == '.':
+                result['result'] += '.'
+            elif token in ('гл', 'ст'):  # Цар. гл. 13 == Цар. 13
+                index += 1
+            elif token == 'и':
+                result['result'] += ','
+            elif token == ';':
+                result['result'] += ';'
+            elif token.isdecimal():
+                result['result'] += token
+            elif token == ',':
+                if ':' not in result['result']:
+                    result['result'] += ':'
+                elif ';' in result['result']:
+                    result['result'] += ':'
+                else:
+                    result['result'] += ','
+            elif re.match(r'\d+–\d+', token):
+                result['result'] += token
+            elif re.match(r'\d+,\d+', token):
+                result['result'] += token.replace(',', ':')
             else:
-                print(words[:i+10])
+                break
+
+        result['final_index'] = index - 3
+        for symbol in '.,:':
+            result['result'] = result['result'].rstrip(symbol)
+
+        return result
+
+    keywords = set(CANONIC_DICT.keys())
+    keywords.update(CANONIC_DICT.values())
+
+    offset = 0
+    word_spans = []
+    for token in nltk.word_tokenize(text):
+        offset = text.find(token, offset)
+        word_spans.append((token, offset, offset + len(token)))
+        offset += len(token)
+
+    changes = []
+    for i, word_span in enumerate(word_spans):
+        if word_span[0] in keywords:
+            st_in = i - 2
+            end_in = i + 30
+
+            result = format([x[0] for x in word_spans[st_in:end_in]], 2)
+
+            if result:
+                st_offset = word_spans[i + result['start_index'] - 3][1]  # offsets are positions in text
+                end_offset = word_spans[i + result['final_index']][2]  # indexes are numbers of tokens
+                logging.info('[CHANGED] %s -> %s', text[st_offset:end_offset], result['result'])
+                logging.debug('[DEBUG] %s ', result)
+                changes.append((st_offset, end_offset, result['result']))
 
     return text
